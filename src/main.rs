62be777021ee668dbf724aea55c13252b41120b3
Milco van Klingeren <mvklingeren@Intersoftware.nl>
2022-09-17 12:49:52 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
 use linfa::prelude::*;
 use linfa_linear::LinearRegression;
+use ndarray::{Array2, Array1, ArrayBase, Dim};
 use rand::Rng;
 
 fn main() {
     /* Generate Training Set */
     let mut rng = rand::thread_rng();
 
-    let train_set_limit = 1000;
+    let train_set_limit:f32 = 1000.0;
     let train_set_count = 100;
 
-    let mut train_input: Vec<[i32; 3]> = Vec::new();
-    let mut train_output: Vec<i32> = Vec::new();
-    for _x in 1..train_set_count {
-        let a: i32 = rng.gen_range(0..train_set_limit); //randint(0, TRAIN_SET_LIMIT)
-        let b = rng.gen_range(0..train_set_limit); //randint(0, TRAIN_SET_LIMIT)
-        let c = rng.gen_range(0..train_set_limit); //randint(0, TRAIN_SET_LIMIT)
-        let op = a + (2 * b) + (3 * c);
+    let mut train_input= Array2::<f32>::zeros((100,3));
+    let mut train_output = Array1::<f32>::zeros(100);
 
-        train_input.push([a, b, c]);
-        train_output.push(op);
+    for i in 1..train_set_count {
+        let a: f32 = rng.gen_range(0.0..train_set_limit);
+        let b = rng.gen_range(0.0..train_set_limit);
+        let c = rng.gen_range(0.0..train_set_limit);
+        let op = a + (2.0 * b) + (3.0 * c);
+
+        train_input[[i,0]] = a;
+        train_input[[i,1]] = b;
+        train_input[[i,2]] = c;
+
+        train_output[i] = op;
     }
 
+    // let records = array![[1., 1.], [2., 2.], [3., 3.], [4., 4.], [5., 5.]];
+    // let targets = array![1., 2., 3., 4., 5.];
+
     /* The ML Model -Linear Regression */
-    let predictor = LinearRegression::default().fit();
+    let dataset = DatasetBase::new(train_input, train_output);
+    let model = LinearRegression::default().fit(&dataset).unwrap();
+ 
+    
     //let predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
 }
